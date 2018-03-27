b3328f21bec898a449f8b68788aad88017da90c5
luffyluffy2018 <abumeis1991@gmail.com>
2018-03-27 19:40:39 +0200

    added x as variabals

--- rien qui march.py
+++ rien qui march.py
@@ -1,1 +1,8 @@
-print ("rien qui marche")
+x = int(input()); y = 3;
+if x > 0 :
+    y = y + x ; print("x is positive")
+    print(y)
+else :
+    y = y -x
+    print("x is negative")
+    print("x is a variabals")
